355068ef95143b92618fec9c8547b161f82a68fd
Andrew <andrepedro62442@gmail.com>
2020-02-27 16:15:03 +0000

    Add XML documentation

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,16 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// My first C# Program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Program starts here.
+        /// </summary>
+        /// <param name="args"></param>
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
